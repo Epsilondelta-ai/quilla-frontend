86816e990a42f94d72ba3b220cfdbc7f9c3189d0
Juunini <juuni.ni.i@gmail.com>
2025-03-19 09:10:02 +0900

    add `SectorContainer` component

---
+++ src/lib/components/Sector/SectorContainer.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	interface Props {
+		class?: string;
+		children?: any;
+	}
+	let { children, ...props }: Props = $props();
+</script>
+
+<article class="mx-auto px-[1.5rem] md:w-[720px] md:px-0 lg:w-[960px] xl:w-[1100px] {props.class}">
+	{@render children?.()}
+</article>
