7fc7a760eb1772d3f62895196996171c4948b563
Juunini <juuni.ni.i@gmail.com>
2025-03-31 15:52:28 +0900

    add height prop in SpeechBubbleArea component

--- src/lib/components/SpeechBubbleArea.svelte
+++ src/lib/components/SpeechBubbleArea.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
-	let { children } = $props();
+	interface Props {
+		children: any;
+		height?: string;
+	}
+
+	let { children, height = 'calc(100vh-200px)' }: Props = $props();
 </script>
 
-<div class="flex max-h-[calc(100vh-200px)] max-w-full flex-col gap-4 overflow-y-auto">
+<div class="flex max-w-full flex-col gap-4 overflow-y-auto" style="height: {height};">
 	{@render children?.()}
 </div>
