cbaa1a99225284436225957bc01d4e8f14715a8c
Juunini <juuni.ni.i@gmail.com>
2025-03-31 13:15:20 +0900

    add Pill component

---
+++ src/lib/components/Pill.stories.svelte
@@ -0,0 +1,23 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+
+	import Pill from './Pill.svelte';
+
+	const { Story } = defineMeta({
+		title: 'components/Pill',
+		component: Pill,
+		tags: ['autodocs']
+	});
+</script>
+
+<Story name="pills">
+	<Pill>Default Pill</Pill>
+	<Pill color="bg-red-500">Red Pill</Pill>
+	<Pill color="bg-green-500">Green Pill</Pill>
+	<Pill color="bg-yellow-500">Yellow Pill</Pill>
+	<Pill color="bg-purple-500">Purple Pill</Pill>
+	<Pill color="bg-pink-500">Pink Pill</Pill>
+	<Pill color="bg-gray-500">Gray Pill</Pill>
+	<Pill color="bg-orange-500">Orange Pill</Pill>
+	<Pill color="bg-teal-500">Teal Pill</Pill>
+</Story>
---
+++ src/lib/components/Pill.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	interface Props {
+		children: any;
+		color?: string;
+	}
+
+	let { children, color = 'bg-blue-500' }: Props = $props();
+</script>
+
+<span class="inline-block rounded-full px-4 py-1 text-sm font-medium text-white {color}">
+	{@render children?.()}
+</span>
