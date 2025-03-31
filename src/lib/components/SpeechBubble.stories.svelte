bdda3d98602aeceec400f624ae7ee1ee6b4cd6df
Juunini <juuni.ni.i@gmail.com>
2025-03-31 13:01:44 +0900

    add SpeechBubble component

---
+++ src/lib/components/SpeechBubble.stories.svelte
@@ -0,0 +1,44 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+
+	import SpeechBubbleArea from './SpeechBubbleArea.svelte';
+	import SpeechBubble from './SpeechBubble.svelte';
+
+	const { Story } = defineMeta({
+		title: 'components/SpeechBubble',
+		component: SpeechBubble,
+		tags: ['autodocs']
+	});
+</script>
+
+<Story name="left">
+	<SpeechBubbleArea>
+		<SpeechBubble
+			content={`레퍼런스 URL https://sdfsdinwegoiheawothsadiohasefheowbnikg.asdgiweogn.asdgiweg.ashdi와 함께 내부의 정보를 요약하여 함께 저장하겠습니다.
+
+URL 내부 컨텐츠 요약
+
+# 인스타그램 브랜드별 전략
+
+|no|브랜드|주요전략|
+|---|---|---|
+|1|스타벅스|고객 참여 유도|
+|2|나이키|브랜드 스토리텔링|
+|3|코카콜라|소셜 미디어 캠페인|
+|4|애플|비주얼 스토리텔링|
+
+이들 브랜드는 각각의 고유한 전략을 통해 인스타그램에서 성공적인 마케팅을 펼치고 있습니다. 스타벅스는 고객 참여를 유도하는 캠페인을 통해 브랜드 충성도를 높이고 있으며, 나이키는 브랜드 스토리텔링을 통해 소비자와의 감정적 연결을 강화하고 있습니다. 코카콜라는 소셜 미디어 캠페인을 통해 소비자와의 소통을 활발히 하고 있으며, 애플은 비주얼 스토리텔링을 통해 제품의 매력을 극대화하고 있습니다.
+`}
+			direction="left"
+		/>
+	</SpeechBubbleArea>
+</Story>
+
+<Story name="right">
+	<SpeechBubbleArea>
+		<SpeechBubble
+			content="인스타그램 마케팅 컨텐츠 https://sdfsdinwegoiheawothsadiohasefheowbnikg.asdgiweogn.asdgiweg.ashdi 저장해줘"
+			direction="right"
+		/>
+	</SpeechBubbleArea>
+</Story>
---
+++ src/lib/components/SpeechBubble.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import { marked } from 'marked';
+
+	// Configure marked options
+	marked.setOptions({
+		breaks: true // Enable line breaks
+	});
+
+	interface Props {
+		content: string;
+		direction: 'left' | 'right';
+	}
+
+	let { content, direction }: Props = $props();
+</script>
+
+<div class="my-2 flex {direction === 'left' ? 'self-start' : 'self-end'}">
+	<div
+		class={`markdown-body relative flex max-w-xs flex-col rounded-lg p-2 text-sm leading-relaxed break-all ${
+			direction === 'left'
+				? 'self-start !bg-gray-200 !text-gray-800'
+				: 'self-end !bg-blue-500 !text-white'
+		}`}
+	>
+		{@html marked(content)}
+		<div
+			class={`absolute h-0 w-0 border-solid ${
+				direction === 'left'
+					? 'top-2.5 left-[-10px] border-t-[10px] border-r-[10px] border-b-[10px] border-l-0 border-transparent border-r-gray-200'
+					: 'top-2.5 right-[-10px] border-t-[10px] border-r-0 border-b-[10px] border-l-[10px] border-transparent border-l-blue-500'
+			}`}
+		></div>
+	</div>
+</div>
